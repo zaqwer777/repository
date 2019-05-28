ff5ebb83fce184b955f25aa0ee8d41f6be458b4a
Albert Xing <albertxing@hotmail.com>
2019-05-28 22:50:36 +0800

    Update AntLabMain.java

--- M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -58,7 +58,7 @@ import edu.gatech.oad.antlab.pkg2.*;
 		  //Person1 replace P1 with your name
 		  //and gburdell1 with your gt id
 		  Person1 p1 = new Person1("Albert Xing");
-		  toPrint += p1.toString("903337032");
+		  toPrint += p1.toString("axing6");
 		  //Person2 replace P2 with your name
 		  //and gburdell with your gt id
 		  Person2 p2 = new Person2("P2");
