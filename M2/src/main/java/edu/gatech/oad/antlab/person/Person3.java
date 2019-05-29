fb2a662246136169ff9eebf90e520a1cac0714af
RunicTable3626 <39796797+RunicTable3626@users.noreply.github.com>
2019-05-29 16:30:03 +0800

    Updated Person3.java
    implemented calc method and also changed author name to "Jay".

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person3.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person3.java
@@ -4,7 +4,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  reversed string 
  *  
- *  @author  Bob
+ *  @author  Jay Jaganaath
  *  @version 1.1
  */
 public class Person3 {
@@ -43,6 +43,10 @@ public class Person3 {
 	 */
 	private String calc(String input) {
 	  //Person 3 put your implementation here
-	  return null;
+	  String toReverse = "";
+	  for( int i = input.length() - 1; i >= 0; i--) {
+		  toReverse += input.charAt(i);
+	  }
+	  return toReverse;
 	}
 }
